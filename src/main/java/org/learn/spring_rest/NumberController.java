5657293ab5829f706ea848fc0ef917ba651c0519
neha-code19 <53231876+neha-code19@users.noreply.github.com>
2024-04-03 18:57:03 -0400

    Testing Code added with MOCKMVC and RESTTEMPLATE

--- src/main/java/org/learn/spring_rest/NumberController.java
+++ src/main/java/org/learn/spring_rest/NumberController.java
@@ -1,8 +1,7 @@
 package org.learn.spring_rest;
 
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
